243f4e069c4c21cabd050235fd903f2f2a4d553f
AmmarElredeny <ammar.foad@afaqy.com>
2020-10-27 08:12:45 +0200

    reset city if user change country in user profile

--- src/app/pages/admin/employees/show-admin-profile/show-admin-profile.component.ts
+++ src/app/pages/admin/employees/show-admin-profile/show-admin-profile.component.ts
@@ -17,6 +17,8 @@ import { NbToastrService } from '@nebular/theme';
 import { environment } from '../../../../../environments/environment';
 import { AuthService } from '../../../../../services/user/auth.service';
 import { CountryService } from '../../../../../services/settings/country.service';
+import { pipe } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-show-admin-profile',
@@ -25,50 +27,53 @@ import { CountryService } from '../../../../../services/settings/country.service
 })
 export class ShowAdminProfileComponent implements OnInit {
   form: FormGroup;
-  roles:any;
-  fileData=null;
-  img:any;
-  showpage:boolean=false;
-  cities:any;
-  country:any;
-  selected:any=[]
-  Data:any;
-  myRoles:any=[];
+  roles: any;
+  fileData = null;
+  img: any;
+  showpage: boolean = false;
+  cities: any;
+  country: any;
+  selected: any = []
+  Data: any;
+  myRoles: any = [];
+  alive: boolean = true;
+  loaded: boolean
   constructor(private formBuilder: FormBuilder,
-    private rolesService:RolesService,
-    private employeeService:EmployeeService,
-    private cityService:CityService,
-    public modalService:BsModalService,
+    private rolesService: RolesService,
+    private employeeService: EmployeeService,
+    private cityService: CityService,
+    public modalService: BsModalService,
     private activeRoute: ActivatedRoute,
-    private languageService:LanguageService,
-    private countryService:CountryService,
-    private localStorageService:LocalStorageService,
-    private notifyService:NbToastrService,
-    public authService:AuthService,
+    private languageService: LanguageService,
+    private countryService: CountryService,
+    private localStorageService: LocalStorageService,
+    private notifyService: NbToastrService,
+    public authService: AuthService,
     private http: HttpClient,
-    ) { }
+  ) { }
 
   ngOnInit() {
-    this.rolesService.GetList().subscribe(res=>{this.roles=res.Data})
-    this.countryService.Getall().subscribe(res=>{this.country=res.Data;})
-    this.rolesService.GetList().subscribe(res=>{this.roles=res.Data})
-    this.cityService.Getall().subscribe(res=>{this.cities=res.Data.data})
+    this.rolesService.GetList().pipe(takeWhile(() => this.alive)).subscribe(res => this.roles = res.Data)
+    this.countryService.Getall().pipe(takeWhile(() => this.alive)).subscribe(res => this.country = res.Data)
+    // this.rolesService.GetList().subscribe(res=>this.roles=res.Data)
+    this.cityService.Getall().pipe(takeWhile(() => this.alive)).subscribe(res => this.cities = res.Data.data)
     this.initForm();
-      this.employeeService.GetByID(+this.activeRoute.snapshot.paramMap.get('id')).subscribe(
-        res=>{
-          res.Data.address=res.Data.admin.address
-          res.Data.firs_tname=res.Data.first_name
-          res.Data.last_name=res.Data.last_name
-          res.Data.city_id=res.Data.city_id
-          res.Data.country_id=res.Data.country_id
-          res.Data.UserID=res.Data.user_id
-        
-          if(res.Data.avatar!=null){
-            this.img=res.Data.avatar
-          }
-        
-         this.form.patchValue(res.Data);
-         res.Data.admin.roles.forEach(element => {
+    this.employeeService.GetByID(+this.activeRoute.snapshot.paramMap.get('id')).pipe(takeWhile(() => this.alive)).subscribe(
+      res => {
+        this.loaded = true;
+        res.Data.address = res.Data.admin.address
+        res.Data.firs_tname = res.Data.first_name
+        res.Data.last_name = res.Data.last_name
+        res.Data.city_id = res.Data.city_id
+        res.Data.country_id = res.Data.country_id
+        res.Data.UserID = res.Data.user_id
+
+        if (res.Data.avatar != null) {
+          this.img = res.Data.avatar
+        }
+
+        this.form.patchValue(res.Data);
+        res.Data.admin.roles.forEach(element => {
           this.selected.push(element.id);
         });
         this.form.get('roles').setValue(this.selected)
@@ -81,59 +86,63 @@ export class ShowAdminProfileComponent implements OnInit {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]); // read file as data url
       this.fileData = <File>event.target.files[0];
-      this.img=this.fileData
-      reader.onload =(event:any) => {
-          this.img=(event.target.result);
-          this.uploadmyImage(this.fileData);
+      this.img = this.fileData
+      reader.onload = (event: any) => {
+        this.img = (event.target.result);
+        this.uploadmyImage(this.fileData);
       }
-}}
-uploadmyImage(Data){
-  const formData = new FormData();
-  formData.append('img', Data);
-  this.http
-    .request(
-      new HttpRequest(
-        'POST',
-        `${environment.api_url}/UploadImage`,
-        formData,
-        { reportProgress: true }
+    }
+  }
+  uploadmyImage(Data) {
+    const formData = new FormData();
+    formData.append('img', Data);
+    this.http
+      .request(
+        new HttpRequest(
+          'POST',
+          `${environment.api_url}/UploadImage`,
+          formData,
+          { reportProgress: true }
+        )
       )
-    )
-    .subscribe(event => {
-      if (event.type === HttpEventType.Response) {
-        if (event.body['Success']) {
+      .subscribe(event => {
+        if (event.type === HttpEventType.Response) {
+          if (event.body['Success']) {
             this.form.get('avatar').setValue(event.body['Data'].image);
-        
-        } else {
-          this.notifyService.danger('something wrong upload again');
-        }
-      }
-    });
-}
 
-changePasswort(){
-  this.modalService.show(ResetPasswordComponent,{ initialState: 
-    { id:+this.activeRoute.snapshot.paramMap.get('id')}, class: 'modal-md' })
-}
+          } else {
+            this.notifyService.danger('something wrong upload again');
+          }
+        }
+      });
+  }
+
+  changePasswort() {
+    this.modalService.show(ResetPasswordComponent, {
+      initialState:
+        { id: +this.activeRoute.snapshot.paramMap.get('id') }, class: 'modal-md'
+    })
+  }
   initForm() {
     this.form = this.formBuilder.group({
       id: [0],
       UserID: [0],
-      type:'admin',
-      gender:[],
-      avatar:[null],
-      email:[null, Validators.required],
-      phone:[null, [Validators.required]],
+      type: 'admin',
+      gender: [],
+      avatar: [null],
+      email: [null, Validators.required, Validators.email],
+      phone: [null, [Validators.required]],
       first_name: [null],
       last_name: [null, Validators.required],
-      address:[null, Validators.required],
-      country_id :[null, Validators.required],
-      city_id:[null, Validators.required],
-      roles:[],
+      address: [null, Validators.required],
+      country_id: [null, Validators.required],
+      city_id: [null, Validators.required],
+      roles: [[]],
     });
-    this.form.get('country_id').valueChanges.subscribe(country=>{
-      this.cityService.GetList(country).subscribe(res=>{this.cities=res.Data})   
-     })
+    this.form.get('country_id').valueChanges.subscribe(country => {
+      this.cityService.GetList(country).subscribe(res => { this.cities = res.Data })
+      this.form.get('city_id').setValue('')
+    })
   }
   save() {
     if (this.form.valid) {
@@ -151,9 +160,9 @@ changePasswort(){
         this.employeeService.Update(this.form.value).subscribe(
           res => {
             if (res.Success) {
-              this.notifyService.success(res.Message,"Sucess");
+              this.notifyService.success(res.Message, "Sucess");
             } else {
-              this.notifyService.danger(res.Message[0],"Error");
+              this.notifyService.danger(res.Message[0], "Error");
             }
           }
         );
