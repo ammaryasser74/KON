b4749a175da5480073fd48189dee7231d51cebdd
AmmarElredeny <ammar.foad@afaqy.com>
2020-11-15 22:29:36 +0200

    fix event reservation get data error

--- src/app/pages/admin/reservation-event/reservation-event.component.ts
+++ src/app/pages/admin/reservation-event/reservation-event.component.ts
@@ -15,14 +15,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./reservation-event.component.scss']
 })
 export class ReservationEventComponent implements OnInit {
-  name='';
-  Data:any;
-  loading:boolean
+  name = '';
+  Data: any;
+  loading: boolean
   addEditaddressModel: BsModalRef;
   warningModel: BsModalRef;
   settings = {
     mode: 'external',
-    hideSubHeader: true ,
+    hideSubHeader: true,
     edit: {
       editButtonContent: '<i class="nb-edit"></i>',
       saveButtonContent: '<i class="nb-checkmark"></i>',
@@ -38,33 +38,33 @@ export class ReservationEventComponent implements OnInit {
     },
     columns: {
       SessionName: {
-        title:this.languageService.getLanguageOrDefault()=='ar'?' اسم المحاضره':"Session Name",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? ' اسم المحاضره' : "Session Name",
         type: 'string',
       },
       CoachName: {
-        title: this.languageService.getLanguageOrDefault()=='ar'?' اسم المدرب':"Coach Name",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? ' اسم المدرب' : "Coach Name",
         type: 'string',
       },
       ClientName: {
-        title: this.languageService.getLanguageOrDefault()=='ar'?' اسم العميل ':"Client Name ",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? ' اسم العميل ' : "Client Name ",
         type: 'string',
       },
       type: {
-        title:this.languageService.getLanguageOrDefault()=='ar'?'نوع الحجز':"Type",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? 'نوع الحجز' : "Type",
         type: 'string',
       },
       date: {
-        title:this.languageService.getLanguageOrDefault()=='ar'?'التاريخ':"Date ",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? 'التاريخ' : "Date ",
         type: 'string',
       }
       ,
       total_price: {
-        title:this.languageService.getLanguageOrDefault()=='ar'?'السعر':"Price ",
+        title: this.languageService.getLanguageOrDefault() == 'ar' ? 'السعر' : "Price ",
         type: 'string',
       }
     },
     actions: {
-      columnTitle: this.languageService.getLanguageOrDefault()=='ar'?' اجراءات':"Actions ",
+      columnTitle: this.languageService.getLanguageOrDefault() == 'ar' ? ' اجراءات' : "Actions ",
       add: false,
       edit: true,
       delete: true,
@@ -75,48 +75,48 @@ export class ReservationEventComponent implements OnInit {
   source: LocalDataSource = new LocalDataSource();
 
   constructor(private service: SmartTableData,
-    private router:Router,
+    private router: Router,
     private toastrService: NbToastrService,
     private modalService: BsModalService,
-    private reservationService:ReservationService,
-    private dialogService:NbDialogService,
-    private languageService:LanguageService) {
+    private reservationService: ReservationService,
+    private dialogService: NbDialogService,
+    private languageService: LanguageService) {
   }
 
-  onEdit(e){
-    this.router.navigate(['/admin/reservation-event/'+e.data.id]);
+  onEdit(e) {
+    this.router.navigate(['/admin/reservation-event/' + e.data.id]);
   }
 
   onDeleteConfirm(event): void {
     this.warningModel = this.modalService.show(WarningComponent, { class: 'modal-sm' });
-    this.warningModel.content.boxObj.msg = this.languageService.getLanguageOrDefault()=='ar'?'انت متاكد من الحذف ؟':'Are you sure you want to delete this  ?';;
+    this.warningModel.content.boxObj.msg = this.languageService.getLanguageOrDefault() == 'ar' ? 'انت متاكد من الحذف ؟' : 'Are you sure you want to delete this  ?';;
     this.warningModel.content.onClose = (cancel) => {
       if (cancel) {
-        this.reservationService.Delete(event.data.id).subscribe(res=>{
-          if(res.Success){
+        this.reservationService.Delete(event.data.id).subscribe(res => {
+          if (res.Success) {
             this.warningModel.hide();
-            this.toastrService.success(res.Message,"success");
+            this.toastrService.success(res.Message, "success");
             this.getData()
           }
-          else{
-            this.toastrService.danger(res.Message,"Error");
+          else {
+            this.toastrService.danger(res.Message, "Error");
           }
         })
       }
     };
   }
 
-  getData(){
-    this.reservationService.GetList().subscribe(res=>{
-      res.Data.EventReservations.map(i=>i.SessionName=i.entity.name_arabic)
-      res.Data.EventReservations.map(i=>i.CoachName=i.coach.user.first_name+' '+i.coach.user.last_name)
-      res.Data.EventReservations.map(i=>i.ClientName=i.client.user.first_name+' '+i.client.user.last_name)
+  getData() {
+    this.reservationService.GetList().subscribe(res => {
+      res.Data.EventReservations.map(i => i.SessionName = i.entity ? i.entity.name_arabic : "")
+      res.Data.EventReservations.map(i => i.CoachName = i.coach.user.first_name + ' ' + i.coach.user.last_name)
+      res.Data.EventReservations.map(i => i.ClientName = i.client.user.first_name + ' ' + i.client.user.last_name)
       this.source.load(res.Data.EventReservations);
-      this.Data=res.Data.EventReservations;
-     
+      this.Data = res.Data.EventReservations;
+
     });
   }
-  
+
   Export() {
     this.reservationService.ExportEvent().subscribe(res => {
       let path = res.Data;
@@ -126,30 +126,29 @@ export class ReservationEventComponent implements OnInit {
 
   addNew() {
     this.router.navigate(['/admin/reservation-event/0']);
-
   }
 
 
   filter(filter: string) {
     if (filter.length) {
-      const asd=(this.Data.filter(i => {
-          let SessionName =  i.entity.name_arabic ;
-          let type =  i.type;
-          let CoachName =   i.CoachName ;
-          let ClientName =  i.ClientName;
-          return `${SessionName ? SessionName.toLowerCase() : ''}
+      const asd = (this.Data.filter(i => {
+        let SessionName = i.entity.name_arabic;
+        let type = i.type;
+        let CoachName = i.CoachName;
+        let ClientName = i.ClientName;
+        return `${SessionName ? SessionName.toLowerCase() : ''}
           ${type ? type.toLowerCase() : ''}
           ${CoachName ? CoachName.toLowerCase() : ''}
           ${ClientName ? ClientName.toLowerCase() : ''}
           `.indexOf(filter.toLowerCase()) !== -1;
-        }))
-        this.source.load(asd);
-      }
-      else{
-        this.source.load(this.Data);
-      }
+      }))
+      this.source.load(asd);
+    }
+    else {
+      this.source.load(this.Data);
+    }
   }
-  ngOnInit(){
+  ngOnInit() {
     this.getData();
   }
 }
